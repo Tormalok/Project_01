4745ccebf3784cd2382cc6a3bcac59fa41fc91a6
Henry <hamponsah010@st.ug.edu.gh>
2024-08-09 13:36:49 +0000

    Added dropdown menus to the nav-bar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import {
+  faSearch,
   faChevronDown,
   faChevronUp,
   faUser,
@@ -10,6 +10,32 @@ import {
 import './styles/navBar.css';
 
 const NavBar = () => {
+  const [openDropdown, setOpenDropdown] = useState(null);
+  const dropdownRef = useRef();
+
+  const handleDropdownToggle = (button) => {
+    if (openDropdown === button) {
+      setOpenDropdown(null);
+    } else setOpenDropdown(button);
+  };
+
+  const handleClickOutside = (event) => {
+    if (
+      dropdownRef.current &&
+      !dropdownRef.current.contains(event.target) &&
+      !event.target.closest('.nav-link')
+    ) {
+      setOpenDropdown(null);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
   return (
     <div className='nav-bar-container'>
       <div className='gp-items-left'>
@@ -26,22 +52,63 @@ const NavBar = () => {
             <FontAwesomeIcon icon={faUser} className='user-icon' />
             <p>Sign in</p>
           </li>
-          <li className='nav-link'>
+          <li
+            className='nav-link'
+            onClick={() => handleDropdownToggle('categories')}
+          >
             <p>Categories</p>
-            <FontAwesomeIcon icon={faChevronDown} className='chevron-icon' />
+            <FontAwesomeIcon
+              icon={openDropdown === 'categories' ? faChevronUp : faChevronDown}
+              className='chevron-icon'
+            />
           </li>
-          <li className='nav-link'>
+          <li
+            className='nav-link'
+            onClick={() => handleDropdownToggle('deals')}
+          >
             <p>Deals</p>
-            <FontAwesomeIcon icon={faChevronDown} className='chevron-icon' />
+            <FontAwesomeIcon
+              icon={openDropdown === 'deals' ? faChevronUp : faChevronDown}
+              className='chevron-icon'
+            />
           </li>
-          <li className='nav-link'>
+          <li
+            className='nav-link'
+            onClick={() => handleDropdownToggle('orders')}
+          >
             <p>Orders</p>
-            <FontAwesomeIcon icon={faChevronDown} className='chevron-icon' />
+            <FontAwesomeIcon
+              icon={openDropdown === 'orders' ? faChevronUp : faChevronDown}
+              className='chevron-icon'
+            />
           </li>
           <div className='cart-icon-container'>
             <FontAwesomeIcon icon={faShoppingCart} className='cart-icon' />
           </div>
         </ul>
+
+        {/* Dropdown Menus */}
+        {openDropdown === 'categories' && (
+          <div className='categories-dropdown-menu' ref={dropdownRef}>
+            <ul>
+              <li>Football</li>
+              <li>F1</li>
+              <li>Cricket</li>
+              <li>Rugby Union</li>
+              <li>Rugby League</li>
+              <li>Golf</li>
+              <li>Boxing</li>
+              <li>Olympics</li>
+              <li>NFL</li>
+              <li>Tennis</li>
+              <li>Racing</li>
+              <li>Darts</li>
+              <li>Netball</li>
+              <li>MMA</li>
+              <li>More Sports</li>
+            </ul>
+          </div>
+        )}
       </div>
     </div>
   );
