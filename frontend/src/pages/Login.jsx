8d7da8c4f0eea6bceabf7f0b73c9c939ff180524
Henry Amponsah <hamponsah010@st.ug.edu.gh>
2024-08-14 07:40:18 +0000

    Improved functionality of the login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import RootBar from '../components/Globals/RootBar';
 import './styles/login.css';
 
 const Login = () => {
+  const [isSignUp, setIsSignUp] = useState(false);
+
+  const toggleForm = (e) => {
+    e.preventDefault();
+    setIsSignUp(!isSignUp);
+  };
+
   return (
     <div className='login-container-main'>
       <RootBar />
       <div className='login-container-inner'>
         <div className='form-box'>
           <div className='form-title'>
-            <h1>Welcome back!</h1>
+            <h1>{isSignUp ? 'Create an account' : 'Welcome back'}</h1>
           </div>
           <form className='login-form'>
             <div className='form-group'>
@@ -31,11 +38,16 @@ const Login = () => {
               />
             </div>
             <button type='submit' className='login-button'>
-              Sign In
+              {isSignUp ? 'Sign Up' : 'Sign In'}
             </button>
 
             <div className='alt-one'>
-              Don't have an account? <span>Sign up</span>
+              {isSignUp
+                ? 'Already have an account? '
+                : "Don't have an account? "}
+              <a href='#' onClick={toggleForm}>
+                {isSignUp ? 'Sign In' : 'Sign Up'}
+              </a>
             </div>
 
             <div className='alt-two'>
@@ -45,9 +57,12 @@ const Login = () => {
             <a href='#' className='account-option-link'>
               <div className='account-option'>
                 <div className='brand-logo-container'>
-                  <img src='/google-icon.svg' className='logo' />
+                  <img
+                    src='/google-icon.svg'
+                    className='logo'
+                    alt='Google logo'
+                  />
                 </div>
-
                 <div className='brand-logo-action'>Continue with Google</div>
               </div>
             </a>
@@ -55,7 +70,11 @@ const Login = () => {
             <a href='#' className='account-option-link'>
               <div className='account-option'>
                 <div className='brand-logo-container'>
-                  <img src='/microsoft-icon.svg' className='logo' />{' '}
+                  <img
+                    src='/microsoft-icon.svg'
+                    className='logo'
+                    alt='Microsoft logo'
+                  />
                 </div>
                 <div className='brand-logo-action'>Continue with Microsoft</div>
               </div>
