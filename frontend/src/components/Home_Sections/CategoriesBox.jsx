7786062ec259a889aa2e2438f9371a4a52ca780b
Henry Amponsah <hamponsah010@st.ug.edu.gh>
2024-08-16 08:29:12 +0000

    Updated the categories box in the homepage

--- frontend/src/components/Home_Sections/CategoriesBox.jsx
+++ frontend/src/components/Home_Sections/CategoriesBox.jsx
@@ -6,19 +6,27 @@ const CategoriesBox = () => {
     <div className='section-two'>
       <div className='box'>
         <div className='box-inner-top'></div>
-        <div className='box-inner-bottom'></div>
+        <div className='box-inner-bottom'>
+          <p>Category 1</p>
+        </div>
       </div>
       <div className='box'>
         <div className='box-inner-top'></div>
-        <div className='box-inner-bottom'></div>
+        <div className='box-inner-bottom'>
+          <p>Category 2</p>
+        </div>
       </div>
       <div className='box'>
         <div className='box-inner-top'></div>
-        <div className='box-inner-bottom'></div>
+        <div className='box-inner-bottom'>
+          <p>Category 3</p>
+        </div>
       </div>
       <div className='box'>
         <div className='box-inner-top'></div>
-        <div className='box-inner-bottom'></div>
+        <div className='box-inner-bottom'>
+          <p>Category 4</p>
+        </div>
       </div>
     </div>
   );
